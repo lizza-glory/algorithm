7cdc7aa909375aefc87d97c2c326696cc85c1155
lizza <lizza1643@gmail.com>
2023-03-12 20:26:40 +0800

    commit

---
+++ Algorithm/src/main/java/com/lizza/BitOpt/BitOpt.java
@@ -0,0 +1,13 @@
+package com.lizza.BitOpt;
+
+import org.junit.Test;
+
+public class BitOpt {
+
+    @Test
+    public void test1() {
+        System.out.println(3 % 2);
+        System.out.println(3 & 1);
+        System.out.println((5 >> 1) + 1);
+    }
+}
