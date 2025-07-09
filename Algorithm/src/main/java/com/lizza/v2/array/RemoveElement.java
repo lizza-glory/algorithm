3a8186cac33b6b69012d9382ecbb5cce6db993ae
lizza <lizza1643@gmail>
2025-07-09 23:49:53 +0800

    commit

--- Algorithm/src/main/java/com/lizza/v2/array/RemoveElement.java
+++ Algorithm/src/main/java/com/lizza/v2/array/RemoveElement.java
@@ -1,6 +1,17 @@
 package com.lizza.v2.array;
 
 /**
+ * 题目:
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
+ *
+ * 假设 nums 中不等于 val 的元素数量为 k，要通过此题，您需要执行以下操作：
+ *
+ * 更改 nums 数组，使 nums 的前 k 个元素包含不等于 val 的元素。nums 的其余元素和 nums 的大小并不重要。
+ * 返回 k。
+ *
+ * 链接: https://leetcode.cn/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * 解法:
  * 如果右指针指向的元素不等于 val，它一定是输出数组的一个元素，我们就将右指针指向的元素复制到左指针位置，
  * 然后将左右指针同时右移；
  *
